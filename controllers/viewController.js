edeaf300f532dd7bf0e799b8206a1010b5f9fb2b
nqan1409 <quangtrungnguyen05021973@gmail.com>
2021-12-28 20:58:46 +0700

    add view login & sign up

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,14 @@ exports.getSignupForm = (req, res) => {
 		layout: 'login-layout',
 	});
 };
-exports.getLoginForm = (req, res) => {
-	res.status(200).render('login', {
+exports.getLogin1Form = (req, res) => {
+	res.status(200).render('login-1', {
+		title: 'Log into your account',
+		layout: false
+	});
+};
+exports.getLogin2Form = (req, res) => {
+	res.status(200).render('login-2', {
 		title: 'Log into your account',
 		layout: false
 	});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,8 @@ const viewController = require('../controllers/viewController');
 const router = express.Router();
 
 router.get('/', viewController.getLoginForm);
+router.get('/login-1', viewController.getLogin1Form);
+router.get('/login-2', viewController.getLogin2Form);
 router.get('/sign-up', viewController.getSignupForm);
 router.get('/index', viewController.getIndex);
 module.exports = router;
