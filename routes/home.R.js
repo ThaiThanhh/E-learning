b12b307ff2d2029046309767c9fa2e0f449259f0
ThaiThanh <Thaithanh11223@gmail.com>
2021-12-31 22:22:46 +0700

    config route

--- routes/home.R.js
+++ routes/home.R.js
@@ -2,7 +2,12 @@ const sign_inController = require('../controllers/home.C')
 const express = require('express')
 const route = express.Router()
 const homeController = require('../controllers/home.C')
+const viewController = require('../controllers/viewController')
 
 route.get('/', homeController.getHome)
+route.get('/dis-courses', viewController.getDisableCourse);
+route.get('/en-courses', viewController.getEnableCourse);
+route.get('/list-courses', viewController.getListCourse);
+route.get('/search-courses', viewController.getSearchCourse);
 
 module.exports = route
--- routes/index.R.js
+++ routes/index.R.js
@@ -2,6 +2,7 @@ const sign_inRoute =  require('./sign_in.R')
 const sign_upRoute =  require('./sign_up.R')
 const homeRoute = require('./home.R')
 function Route(app){
+    
     app.use('/', homeRoute)
     app.use('/signin', sign_inRoute)
     app.use('/signup', sign_upRoute)
