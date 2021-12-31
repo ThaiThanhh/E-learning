4c35e1524ae17c84cea78a2df236b331cc7db01b
nqan1409 <quangtrungnguyen05021973@gmail.com>
2021-12-31 19:31:45 +0700

    add views courses

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,6 +4,30 @@ exports.getIndex =  (req, res) => {
 		layout: 'main',
 	});
 };
+exports.getDisableCourse =  (req, res) => {
+	res.status(200).render('dis-courses', {
+		title: 'Disable courses',
+		layout: 'main',
+	});
+};
+exports.getEnableCourse =  (req, res) => {
+	res.status(200).render('en-courses', {
+		title: 'Enable courses',
+		layout: 'main',
+	});
+};
+exports.getListCourse =  (req, res) => {
+	res.status(200).render('list-courses', {
+		title: 'List courses',
+		layout: 'main',
+	});
+};
+exports.getSearchCourse =  (req, res) => {
+	res.status(200).render('search-courses', {
+		title: 'Search courses',
+		layout: 'main',
+	});
+};
 exports.getSignupForm = (req, res) => {
 	res.status(200).render('signup', {
 		title: 'Register new account',
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,10 @@ const viewController = require('../controllers/viewController');
 
 const router = express.Router();
 
+router.get('/dis-courses', viewController.getDisableCourse);
+router.get('/en-courses', viewController.getEnableCourse);
+router.get('/list-courses', viewController.getListCourse);
+router.get('/search-courses', viewController.getSearchCourse);
 router.get('/login-1', viewController.getLogin1Form);
 router.get('/login-2', viewController.getLogin2Form);
 router.get('/sign-up', viewController.getSignupForm);
